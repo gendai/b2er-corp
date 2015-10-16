5fb2836ffda97e112c81df1487a36487104475ba
Steven Lariau <obs145628@gmail.com>
2015-10-16 14:52:44 +0200

    Adding file dialog for chosing file image

--- obstest/main.c
+++ obstest/main.c
@@ -10,16 +10,38 @@ static void activate (GtkApplication *app, gpointer user_data)
 	(void)(user_data);
 	
 	GtkWidget *window;
-        GtkWidget *image = gtk_image_new_from_file("../misc/sequei_i.jpg");
 
 	window = gtk_application_window_new (app);
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
 	gtk_window_set_default_size (GTK_WINDOW (window), 1500, 1000);
+
 	
-	StrongClassifier *sc = StrongClassifierNewFromFile("../misc/classifier.data");
-	detectFaces((GtkImage *) image, sc);
+	GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File",
+													(GtkWindow *) window,
+													GTK_FILE_CHOOSER_ACTION_OPEN,
+													"Cancel",
+													GTK_RESPONSE_CANCEL,
+													"Open",
+													GTK_RESPONSE_ACCEPT,
+													NULL);
+
+	GtkFileChooser *chooser  = GTK_FILE_CHOOSER(dialog);
+
+	gint res = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if(res == GTK_RESPONSE_ACCEPT)
+	{
+		char *imagePath = gtk_file_chooser_get_filename(chooser);
+		gtk_widget_destroy(dialog);
+
+		GtkWidget *image = gtk_image_new_from_file(imagePath);
+		StrongClassifier *sc = StrongClassifierNewFromFile("../misc/classifier.data");
+		detectFaces((GtkImage *) image, sc);
+		gtk_container_add (GTK_CONTAINER (window), image);
+	}
+
 	
-	gtk_container_add (GTK_CONTAINER (window), image);
+	
 	gtk_widget_show_all (window);
 }
 
